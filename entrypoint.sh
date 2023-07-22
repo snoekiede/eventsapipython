db877c6cdd6e1a2b24949bcb63d74c11eb851511
Iede Snoek <info@esoxsolutions.nl>
2023-07-22 15:20:50 +0200

    Added gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME','$DJANGO_SUPER_USER_EMAIL','$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell 
-python manage.py runserver 0.0.0.0:8000
+CMD ["gunicorn","--bind",":8000","--workers","3","--threads","2","--timeout","90","--preload","config.wsgi:application","webevents.wsgi"]
 
