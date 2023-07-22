434faf3f5b8a970ac58fe7fa85b2a238e2ed3807
Iede Snoek <info@esoxsolutions.nl>
2023-07-22 11:08:45 +0200

    Changed CRLF to LF

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,4 @@
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME','$DJANGO_SUPER_USER_EMAIL','$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell 
 python manage.py runserver 0.0.0.0:8000
+
