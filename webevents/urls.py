cc9ac98cdc8a36fc39454b03bce82877315effad
Iede Snoek <info@esoxsolutions.nl>
2023-07-23 13:02:39 +0200

    Now works correctly with gunicorn

--- webevents/urls.py
+++ webevents/urls.py
@@ -21,9 +21,12 @@ Including another URLconf
 #     path('admin/', admin.site.urls),
 # ]
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns=[
     path('admin/', admin.site.urls),
     path('', include('eventsapi.urls')),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
